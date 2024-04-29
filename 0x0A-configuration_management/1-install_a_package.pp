15ea6c653d623f97eeff1cae0952d06672c1d5a3
Terry_rose <2216mayet@gmail.com>
2024-04-29 14:37:02 -0700

    commit

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/bin', '/usr/bin'],
+  environment => [],
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require     => Package['python3-pip'],
+}
+
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+exec { 'killmenow':
+  command     => '/usr/bin/pkill -f killmenow',
+  path        => ['/bin', '/usr/bin'],
+  onlyif      => '/usr/bin/pgrep -f killmenow',
+}
+
